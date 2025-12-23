bb2cc31f8480cc2766f951f07adcc79d8ad19e97
Sebastien Boulas <43757451+boulas627@users.noreply.github.com>
2025-12-22 23:56:18 -0500

    Update UserController.java

--- src/main/java/com/example/vulnapp/controller/UserController.java
+++ src/main/java/com/example/vulnapp/controller/UserController.java
@@ -10,6 +10,8 @@ public class UserController {
 
     @GetMapping("/eval")
     public String eval(@RequestParam String expression) {
+
+        // The line of code below is vulnerable to a spring expression injection vulnerability
         ExpressionParser parser = new SpelExpressionParser();
         return parser.parseExpression(expression).getValue().toString();
     }
