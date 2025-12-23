ebddf3c60379047ec34fba8e1fea1a8f8d6d2040
Sebastien Boulas <43757451+boulas627@users.noreply.github.com>
2025-12-22 23:52:44 -0500

    Update VulnAppApplication.java

--- src/main/java/com/example/vulnapp/VulnAppApplication.java
+++ src/main/java/com/example/vulnapp/VulnAppApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VulnAppApplication {
     public static void main(String[] args) {
+
+        // Arguments passed below are user controlled and makes this line potentially vulnerable to some time of an injection vulnerability
         SpringApplication.run(VulnAppApplication.class, args);
     }
 }
