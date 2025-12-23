42ad53e48eec40e4b2c5639de6f5b49026c98e18
Sebastien Boulas <43757451+boulas627@users.noreply.github.com>
2025-12-22 23:46:31 -0500

    Update DeserializeUtil.java

--- src/main/java/com/example/vulnapp/util/DeserializeUtil.java
+++ src/main/java/com/example/vulnapp/util/DeserializeUtil.java
@@ -5,6 +5,8 @@ import java.io.*;
 
 public class DeserializeUtil {
     public static Object deserialize(byte[] data) throws Exception {
+
+        // The line of code down below shows potential command injection due to the data field being user controlled. Deserializing data without proper sanitization could be reckless here. 
         ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data));
         return ois.readObject();
     }
