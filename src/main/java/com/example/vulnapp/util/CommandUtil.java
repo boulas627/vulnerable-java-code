ea678f34da060ff21ace8a95c9908f03bba8b2ac
Sebastien Boulas <43757451+boulas627@users.noreply.github.com>
2025-12-22 23:45:07 -0500

    Update CommandUtil.java

--- src/main/java/com/example/vulnapp/util/CommandUtil.java
+++ src/main/java/com/example/vulnapp/util/CommandUtil.java
@@ -3,6 +3,8 @@ package com.example.vulnapp.util;
 
 public class CommandUtil {
     public static String ping(String host) throws Exception {
+
+        // The code in the line below is a command injection vulnerability. The variable host is user controlled
         Process p = Runtime.getRuntime().exec("ping -c 1 " + host);
         return new String(p.getInputStream().readAllBytes());
     }
