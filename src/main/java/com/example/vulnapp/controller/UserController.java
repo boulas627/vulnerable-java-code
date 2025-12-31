de7da3215a88e717f484b35074c99515becd7cda
Sebastien Boulas <43757451+boulas627@users.noreply.github.com>
2025-12-30 20:22:39 -0500

    Update UserController.java

--- src/main/java/com/example/vulnapp/controller/UserController.java
+++ src/main/java/com/example/vulnapp/controller/UserController.java
@@ -11,6 +11,22 @@ public class UserController {
     @GetMapping("/eval")
     public String eval(@RequestParam String expression) {
         ExpressionParser parser = new SpelExpressionParser();
-        return parser.parseExpression(expression).getValue().toString();
+
+        # the fix below assumes that SpEL usage is required and imposes various restrictions on its usage. The best case scenario would be to remove it entirely if possible. 
+
+        StandardEvaluationContext context = new StandardEvaluationContext();
+
+        context.setTypeLocator(typeName -> {
+            throw new EvaluationException("Type access is not allowed");
+        });
+
+        context.setMethodResolvers(Collections.emptyList());
+
+        context.setPropertyAccessors(Collections.emptyList());
+
+        Object value = parser.parseExpression(expression).getValue(context);
+        
+        return String.valueOf(value);
+
     }
 }
