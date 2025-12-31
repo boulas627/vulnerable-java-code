ac42ac4907dd3995f07be2ff3964be5adcc6f878
Sebastien Boulas <43757451+boulas627@users.noreply.github.com>
2025-12-30 20:29:53 -0500

    Update DeserializeUtil.java

--- src/main/java/com/example/vulnapp/util/DeserializeUtil.java
+++ src/main/java/com/example/vulnapp/util/DeserializeUtil.java
@@ -6,6 +6,13 @@ import java.io.*;
 public class DeserializeUtil {
     public static Object deserialize(byte[] data) throws Exception {
         ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data));
+
+        ObjectInputFilter filter = ObjectInputFilter.Config.createFilter(
+            "com.example.vulnapp.model.*;java.base/*;!*"
+        );
+
+        ois.setObjectInputFilter(filter);
+        
         return ois.readObject();
     }
 }
