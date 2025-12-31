9f2f244daca0adb119b6209c8f87b405113008a2
Sebastien Boulas <43757451+boulas627@users.noreply.github.com>
2025-12-30 20:01:27 -0500

    Update CommandUtil.java

--- src/main/java/com/example/vulnapp/util/CommandUtil.java
+++ src/main/java/com/example/vulnapp/util/CommandUtil.java
@@ -3,7 +3,15 @@ package com.example.vulnapp.util;
 
 public class CommandUtil {
     public static String ping(String host) throws Exception {
-        Process p = Runtime.getRuntime().exec("ping -c 1 " + host);
+
+        if (!host.matches("^[a-zA-Z0-9.-]+$")) {
+            throw new IllegalArgumentException("Invalid host");
+        }
+        
+        Process p = new ProcessBuilder(
+            "ping", "-c", "1", host
+            ).start(); 
+
         return new String(p.getInputStream().readAllBytes());
     }
 }
