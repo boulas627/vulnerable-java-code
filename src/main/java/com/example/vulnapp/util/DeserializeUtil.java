bfb7405d0ef4fad9af19cc61cbbce71cfa60fe1d
Sebastien Boulas <43757451+boulas627@users.noreply.github.com>
2025-12-30 20:30:45 -0500

    Update DeserializeUtil.java

--- src/main/java/com/example/vulnapp/util/DeserializeUtil.java
+++ src/main/java/com/example/vulnapp/util/DeserializeUtil.java
@@ -7,6 +7,8 @@ public class DeserializeUtil {
     public static Object deserialize(byte[] data) throws Exception {
         ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data));
 
+        # the code fixes below assume that serialization is absolutely required. Ideally, we would avoid it entirely
+
         ObjectInputFilter filter = ObjectInputFilter.Config.createFilter(
             "com.example.vulnapp.model.*;java.base/*;!*"
         );
